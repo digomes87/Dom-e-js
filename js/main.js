4d104d41fb5d232e1a3c56c655dc1b6b91f68455
Diego <diego.gomes87@gmail.com>
2019-06-24 23:36:24 -0300

    usando axios para requisicao

--- js/main.js
+++ js/main.js
@@ -1,36 +1,4 @@
-// var xhr = new XMLHttpRequest();
-
-
-// xhr.open('GET', 'https://api.github.com/users/digomes87');
-// xhr.send(null);
-
-
-// xhr.onreadystatechange = function(){
-//     if(xhr.readyState === 4){
-//         console.log(JSON.parse(xhr.responseText));
-//     }
-// }
-
-var minhaPromese = function(){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET','https://api.github.com/users/digomes87');
-        xhr.send(null);
-
-
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else{
-                    reject('Erro no JSON');
-                }
-            }
-        }
-    });
-}
-
-minhaPromese()
+axios.get('https://api.github.com/users/digomes87')
     .then(function(response){
         console.log(response);
     })
