413f4e64fed7b9dd91c1abaa500e42b1ec800663
Diego Go <Diego.Gomes87@gmail.com>
2019-06-24 22:55:59 -0300

    Mudando o Ajax, usando biblioteca + simples

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
 
 
-xhr.open('GET', 'https://api.github.com/users?since=135');
-xhr.send(null);
+// xhr.open('GET', 'https://api.github.com/users?since=135');
+// xhr.send(null);
 
 
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
+// xhr.onreadystatechange = function(){
+//     if(xhr.readyState === 4){
+//         console.log(JSON.parse(xhr.responseText));
+//     }
+// }
+
+var minhaPromese = function(){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('https://api.github.com/users?since=135');
+    });
 }
